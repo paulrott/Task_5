4e2172238dcedc0db260855c8d54ac97fb14fe2d
gromov.p <paulrott76@gmail.com>
2021-11-02 22:34:15 +0300

    Task 5

---
+++ Task 5.py
@@ -0,0 +1,37 @@
+greeting = 'Hello Python!'
+greeting_length = len(greeting)
+print(len(greeting))
+
+#Indexing
+print(greeting[1])
+print(greeting[6])
+print(greeting[-1])
+print(greeting[12])
+print(greeting[-4])
+
+#Slicing
+print(greeting[2:5])
+print(greeting[6:10])
+print(greeting[2:5])
+print(greeting[-5:-2])
+print(greeting[2:])
+print(greeting[:5])
+print(greeting[:])
+print(greeting[::2])
+print(greeting[::1])
+print(greeting[1::3])
+print(greeting[1:9:3])
+print(greeting[::-1])
+
+#Task5
+greeting = 'Hello Python!'
+print(greeting[3])
+print('Hello Python!'[3])
+
+print(greeting[0:2])
+print(greeting[:2])
+print(greeting[-13:-11])
+print(greeting[6] + 'a' + greeting[8:10])
+path = greeting[6:7] + 'a' + greeting[8:10]
+print(path)
+
